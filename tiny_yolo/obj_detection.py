1c3b061997aa980fcda71fc3ce69f34c44122f0b
Sanketh Salimath <41461029+sankethsalimath@users.noreply.github.com>
2020-09-20 20:13:12 +0530

    Add files via upload

---
+++ tiny_yolo/obj_detection.py
@@ -0,0 +1,24 @@
+from models import *
+from utils import *
+import os, sys, time, datetime, random
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+
+config_path='config/yolov3.cfg'
+weights_path='config/yolov3.weights'
+class_path='config/coco.names'
+img_size=416
+conf_thres=0.8
+nms_thres=0.4
+# Load model and weights
+model = Darknet(config_path, img_size=img_size)
+model.load_weights(weights_path)
+model.cuda()
+model.eval()
+classes = utils.load_classes(class_path)
+Tensor = torch.cuda.FloatTensor
